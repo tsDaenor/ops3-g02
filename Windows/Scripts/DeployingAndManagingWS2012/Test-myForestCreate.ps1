6298ce908d51d09bef4da6171c556f7de514939f
brammartens <bram.martens.u8474@student.hogent.be>
2015-10-30 11:00:02 +0100

    aanpassing scripts

--- Windows/Scripts/DeployingAndManagingWS2012/Test-myForestCreate.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module ADDSDeployment
-Test-ADDSForestInstallation `
-     -DomainName 'TreyResearch.net' `
-     -DomainNetBiosName 'TREYRESEARCH' `
-     -DomainMode 6 `
-     -ForestMode 6 `
-     -NoDnsOnNetwork `
-     -NoRebootOnCompletion
-
